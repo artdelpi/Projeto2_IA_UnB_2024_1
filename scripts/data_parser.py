fa23898fe5a7831e2dd26e8c1aea4eed5b46465f
artdelpi <artdelpi@gmail.com>
2024-06-23 11:15:42 -0300

    Refatoração geral

--- scripts/data_parser.py
+++ scripts/data_parser.py
@@ -1,46 +1,34 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
-from scipy.sparse import csr_matrix
+from numpy import ndarray
 
-def parse_data(path:str) -> tuple[csr_matrix, csr_matrix, pd.core.series.Series, pd.core.series.Series]:
-    """
-    Carrega dados do arquivo CSV a partir do path fornecido,
-    separa atributos de rótulos, divide dados para teste  e  treinamento e
-    vectoriza textos para serem usados por algoritmos de machine learning.
-    
-    Params:
-    - path (str): O caminho para o arquivo CSV.
-
-    Returns:
-    - texts_train_vec (csr_matrix): Textos vectorizados para treinamento.
-    - texts_test_vec (csr_matrix): Textos vectorizados para teste.
-    - labels_train (Series): Rótulos de treinamento.
-    - labels_test (Series): Rótulos de teste.
-    """
-    # Carrega dados do arquivo CSV
+def parse_data(data_path:str) -> tuple[ndarray, ndarray, pd.core.series.Series, pd.core.series.Series]:
     try:
-        file = pd.read_csv(path)
-    except FileNotFoundError:
-        raise FileNotFoundError(f'Arquivo {path} não encontrado.')
-    except pd.errors.ParserError:
-        raise ValueError(f'Não foi possível analisar o arquivo {path}')
-
-
-    # Separa atributos de rótulos
-    file.dropna(subset=['Text', 'Label'], inplace=True)
-    texts, labels = file['Text'], file['Label']
-
-    # Separa dados para teste e treinamento
-    texts_train, texts_test, labels_train, labels_test = train_test_split(
-        texts, labels, 
-        test_size=0.3, 
-        random_state=42
-    )
+        data_file = pd.read_csv(data_path)
+    except FileNotFoundError as e:
+        print(f'Erro: O arquivo {data_path} não foi encontrado.')
+        raise(e)
+    except pd.errors.EmptyDataError as e:
+        print(f'Erro: O arquivo {data_path} está vazio.')
+        raise(e)
+    except pd.erros.ParserError as e:
+        print(f'Erro: Ocorreu um erro ao parsear o arquivo {data_path}')
+        raise(e)
+    except Exception as e:
+        print(f'Erro: Ocorreu um erro inesperado ao ler o arquivo {data_path}')
+        raise(e)
     
+    # Verifica estrutura esperada do DataFrame ('Text'|'Label')
+    if 'Text' not in data_file.columns or 'Label' not in data_file.columns:
+        raise ValueError('Erro: As colunas "Text" e/ou "Label" não foram encontradas.')
+
     # Vectoriza os textos
     vectorizer = TfidfVectorizer()
-    texts_train_vec = vectorizer.fit_transform(texts_train)
-    texts_test_vec = vectorizer.transform(texts_test)
+    X = vectorizer.fit_transform(data_file['Text']).toarray()
+    y = data_file['Label'].apply(lambda x:1 if x == 'ai' else 0)
 
-    return texts_train_vec, texts_test_vec, labels_train, labels_test
+    # Separa dados pra teste (0.3) e treinamento (0.7)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+    return X_train, X_test, y_train, y_test
--- scripts/model_trainer.py
+++ scripts/model_trainer.py
@@ -1,29 +1,22 @@
 import numpy as np
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis as QDA
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier as KNN
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import roc_auc_score, roc_curve
+from sklearn.metrics import roc_curve, auc, accuracy_score, precision_score, recall_score
 from data_parser import parse_data
+from sklearn import metrics
 
-def train_and_evaluate_classifiers(path):
+def train_and_evaluate_classifiers(data_path):
 
-    # Parse dos dados
-    texts_train_vec, texts_test_vec, labels_train, labels_test = parse_data(path)
-
-    # Verifica e remove valores ausentes
-    labels_train.dropna(inplace=True)
-    labels_test.dropna(inplace=True)
-
-    # Converte rótulos para valores binários
-    labels_train_binary = labels_train.apply(lambda x: 1 if x == 'ai' else 0)
-    labels_test_binary = labels_test.apply(lambda x: 1 if x == 'ai' else 0)
+    # Pré-processa conjunto de dados
+    X_train, X_test, y_train, y_test = parse_data(data_path)
 
     classifiers = {
         'LDA': LDA(),
         'QDA': QDA(),
-        'K-NN': KNeighborsClassifier(),
+        'KNN': KNN(),
         'SVM': SVC(probability=True),
         'Random Forest': RandomForestClassifier()
     }
@@ -31,24 +24,30 @@ def train_and_evaluate_classifiers(path):
     results = {}
 
     for name, clf in classifiers.items():
-        if name in ['LDA', 'QDA']:
-            # Converte para arrays densos
-            texts_train_dense = texts_train_vec.toarray()
-            texts_test_dense = texts_test_vec.toarray()
-            clf.fit(texts_train_dense, labels_train_binary)
-            y_pred_proba = clf.predict_proba(texts_test_dense)[:, 1]
-        else:
-            clf.fit(texts_train_vec, labels_train_binary)
-            y_pred_proba = clf.predict_proba(texts_test_vec)[:, 1]
+        # Treina o modelo
+        clf.fit(X_train, y_train)
 
-        auc_score = roc_auc_score(labels_test_binary, y_pred_proba)
-        fpr, tpr, _ = roc_curve(labels_test_binary, y_pred_proba)
+        # Prediciona probabilidades
+        y_proba = clf.predict_proba(X_test)[:, 1]
 
+        fpr, tpr, _ = roc_curve(y_test, y_proba)
+        roc_auc = auc(fpr, tpr)
+
+        # Prediciona rótulos
+        y_pred = clf.predict(X_test)
+
+        # Calcula métricas de desempenho
+        accuracy = accuracy_score(y_test, y_pred)
+        precision = precision_score(y_test, y_pred)
+        recall = recall_score(y_test, y_pred)
+        
         results[name] = {
-            'classifier': clf,
-            'auc': auc_score,
             'fpr': fpr,
-            'tpr': tpr
+            'tpr': tpr,
+            'auc': roc_auc,
+            'accuracy': accuracy,
+            'precision': precision,
+            'recall': recall
         }
 
     return results
